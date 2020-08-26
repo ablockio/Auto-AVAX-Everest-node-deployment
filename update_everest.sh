e64ba87ad847e91fe0c8002fe2fd19d6772bf0bb
aorfevre <aorfevre@gmail.com>
2020-08-26 16:07:55 +0200

    Update rights to update script

--- update_everest.sh
+++ update_everest.sh
@@ -42,53 +42,50 @@ else
 echo 'systemd is not available on this machine, will use supervisord instead'
 fi
 
+echo '### Creating AVA node service...'
+if [ -n "$SYSTEMD_SUPPORTED" ]; then
+sudo USER=$USER bash -c 'cat <<EOF > /etc/systemd/system/avaxnode.service
+[Unit]
+Description=AVAX Everest Node service
+After=network.target
 
-  echo '### Creating AVAX node service...'
-  if [ -n "$SYSTEMD_SUPPORTED" ]; then
-  sudo rm -f /etc/systemd/system/avaxnode.service
-  sudo USER=$USER bash -c 'cat <<EOF > /etc/systemd/system/avaxnode.service
-  [Unit]
-  Description=AVAX Everest Node service
-  After=network.target
+[Service]
+User=$USER
+Group=$USER
 
-  [Service]
-  User=$USER
-  Group=$USER
+WorkingDirectory=$HOME/avalanche-'$_VERSION'
+ExecStart=$HOME/avalanche-'$_VERSION'/avalanche
 
-  WorkingDirectory=$HOME/avalanche-'$_VERSION'
-  ExecStart=$HOME/avalanche-'$_VERSION'/avalanche
+Restart=always
+PrivateTmp=true
+TimeoutStopSec=60s
+TimeoutStartSec=10s
+StartLimitInterval=120s
+StartLimitBurst=5
 
-  Restart=always
-  PrivateTmp=true
-  TimeoutStopSec=60s
-  TimeoutStartSec=10s
-  StartLimitInterval=120s
-  StartLimitBurst=5
-
-  [Install]
-  WantedBy=multi-user.target
-  EOF'
-  sudo systemctl daemon-reload
-  else
-  sudo rm -f /etc/supervisor/conf.d/avaxnode.conf
-  sudo bash -c 'cat <<EOF > /etc/supervisor/conf.d/avaxnode.conf
-  [program:avaxnode]
-  directory=/home/$SUDO_USER/avalanche-'$_VERSION'
-  command=/home/$SUDO_USER/avalanche-'$_VERSION'/avalanche
-  user=$SUDO_USER
-  environment=HOME="/home/$SUDO_USER",USER="$SUDO_USER"
-  autostart=true
-  autorestart=true
-  startsecs=10
-  startretries=20
-  stdout_logfile=/var/log/avaxnode-stdout.log
-  stdout_logfile_maxbytes=10MB
-  stdout_logfile_backups=1
-  stderr_logfile=/var/log/avaxnode-stderr.log
-  stderr_logfile_maxbytes=10MB
-  stderr_logfile_backups=1
-  EOF'
-  fi
+[Install]
+WantedBy=multi-user.target
+EOF'
+sudo systemctl daemon-reload
+else
+sudo bash -c 'cat <<EOF > /etc/supervisor/conf.d/avaxnode.conf
+[program:avaxnode]
+directory=/home/$SUDO_USER/avalanche-'$_VERSION'
+command=/home/$SUDO_USER/avalanche-'$_VERSION'/avalanche
+user=$SUDO_USER
+environment=HOME="/home/$SUDO_USER",USER="$SUDO_USER"
+autostart=true
+autorestart=true
+startsecs=10
+startretries=20
+stdout_logfile=/var/log/avaxnode-stdout.log
+stdout_logfile_maxbytes=10MB
+stdout_logfile_backups=1
+stderr_logfile=/var/log/avaxnode-stderr.log
+stderr_logfile_maxbytes=10MB
+stderr_logfile_backups=1
+EOF'
+fi
 
 
 echo '### Downloading latest version...'
